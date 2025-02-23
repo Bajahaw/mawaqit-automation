bda623b174490ef31bcccc1350cb5f2f5253118c
bajahaw <rathi7137@gmail.com>
2025-02-23 13:22:01 +0100

    simplified loop logic

--- src/main.py
+++ src/main.py
@@ -6,21 +6,6 @@ from notify import NotificationManager
 from utils.parse_json import parse_json
 
 def schedule_next_job(delay_seconds):
-
-    # check if there is a pending job within plus or minus 5 seconds
-    result = subprocess.run([
-        "termux-job-scheduler",
-        "--pending"
-    ], capture_output=True).stdout.decode("utf-8")
-    
-    if "Pending Job 1" in result:
-        # extract the period value from the result string
-        period = int(result.split("periodic: ")[1].split("ms")[0])
-
-        # if the period is within plus or minus 5 seconds of the delay, do nothing
-        if period - 5000 <= delay_seconds * 1000 <= period + 5000:
-            return
-
     # convert seconds to milliseconds for the scheduling job
     deadline_ms = delay_seconds * 1000
     print(f"Scheduling next job in {int(delay_seconds // 3600)} hours and {int((delay_seconds % 3600) // 60)} mins.")
@@ -38,11 +23,12 @@ def main():
     lock_file = os.path.join(os.path.dirname(__file__), "utils", "lock")
     if os.path.exists(lock_file):
         os.remove(lock_file)
-        print("lock file removed.")
+        print("lock file removed")
         return
 
     else:
         with open(lock_file, "w") as f:
+            print("lock file created")
             f.write("lock")
 
     # build the json file path from the parent data directory
@@ -64,10 +50,9 @@ def main():
         return 
 
     # find the next prayer time
-    for prayer_time in prayer_times:
+    for i in range(len(prayer_times)):
 
-        print(f"next prayer time: {prayer_time}")
-
+        prayer_time = prayer_times[i]
         # parse the prayer time string into a datetime object and update to today's date
         prayer_parsed_time = datetime.datetime.strptime(prayer_time, "%H:%M")
         next_prayer_time = datetime.datetime.combine(now.date(), prayer_parsed_time.time())
@@ -75,6 +60,7 @@ def main():
 
         # check if the current time is before the prayer time
         if now < next_prayer_time:
+            
             # calculate the time diff to next prayer
             time_diff = int((next_prayer_time - now).total_seconds())
             print(f"waiting {int(time_diff // 3600)} hours and {int((time_diff % 3600) // 60)} mins for prayer at {prayer_time} ...")
@@ -82,27 +68,26 @@ def main():
             # schedule a job timer for the next prayer
             schedule_next_job(time_diff)
 
-            if next_prayer_time == datetime.datetime.combine(now.date(), datetime.datetime.strptime(prayer_times[0], "%H:%M").time()):
+            if i == 0:
                 # at day's start, print a welcome message with today’s date
                 print("-------------------------")
                 print("new day, may it be blessed .. date: ", datetime.datetime.now().date())
 
+            else:
+                # send a notification for the upcoming prayer
+                notification_manager.send_notification(
+                    title="upcoming prayer",
+                    content=f"the next prayer is at {prayer_times[i-1]}."
+                )
+                print(f"notification sent: upcoming prayer at {prayer_times[i-1]}")
+
             # break after scheduling the next prayer
             break
 
-        elif next_prayer_time == datetime.datetime.combine(now.date(), datetime.datetime.strptime(prayer_times[5], "%H:%M").time()):
+        elif i == len(prayer_times) - 1: # Ishaa prayer
             # no more prayers today, wait until midnight
             print("no upcoming prayer today. waiting until midnight...")
             schedule_next_job(60 * 60 * (24 - now.hour))
-
-            break
-            
-        # if the current time is within 2 minutes of the prayer, due to scheduler imprecision, send a notification
-        if next_prayer_time + datetime.timedelta(minutes=2) > now > next_prayer_time - datetime.timedelta(minutes=2):
-            notification_manager.send_notification(
-                title="prayer time",
-                content=f"it's time for prayer at {prayer_time}."
-            )
 
 if __name__ == "__main__":
     main()
